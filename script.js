69d8bf7c756ec66f3ae2fabb2ff5d05c5ac6adbd
Jason Sutanto <jasonsutanto665@gmail.com>
2024-03-02 16:29:15 +0700

    CAROUSEL UPDATE

--- script.js
+++ script.js
@@ -67,35 +67,4 @@ function displayFoodData(food) {
 }
 });
 
-$('.carousel-sync').carousel('cycle');
-$('.carousel-sync').on('click', '.carousel-control[data-slide]', function (ev) {
-	ev.preventDefault();
-    $('.carousel-sync').carousel($(this).data('slide'));
-});
-$('.carousel-sync').on('mouseover', function(ev) {
-  	ev.preventDefault();
-    $('.carousel-sync').carousel('pause');
-});
-$('.carousel-sync').on('mouseleave', function(ev) {
-  	ev.preventDefault();
-    $('.carousel-sync').carousel('cycle');
-});
-
-$('#carousel-1').bind('slid.bs.carousel', function (e)
-        {
-          
-            var $this = $(this);
-
-            $this.children('.carousel-control').show();
-
-            if ($('.carousel-inner .item:last').hasClass('active'))
-            {
-               $('#carousel-b').carousel('pause');
-                $this.children('.right.carousel-control').hide();
-            } else if ($('.carousel-inner .item:first').hasClass('active'))
-            {
-                $this.children('.left.carousel-control').hide();
-            }
-        });
-
 
