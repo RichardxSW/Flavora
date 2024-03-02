9503c031d3c6b36ec455e1a180b7ca50151b2ea5
Richard Souwiko <richard.s050804@gmail.com>
2024-03-02 15:02:43 +0700

    js di button pin/unpin

---
+++ nasgorDetail.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btnSave = document.querySelector(".unsaved");
+    const btnUnsave = document.querySelector(".saved");
+
+    btnSave.addEventListener("click", function () {
+        btnSave.style.display = "none";
+        btnUnsave.style.display = "inline-block"
+    })
+
+    btnUnsave.addEventListener("click", function () {
+        btnUnsave.style.display = "none";
+        btnSave.style.display = "inline-block";
+    })
+
+    
+});
