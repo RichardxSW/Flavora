b5496591904ef8ffb5e739016a31bba54ddea3cb
Jason Sutanto <jasonsutanto665@gmail.com>
2024-03-08 22:02:19 +0700

    buggy

--- script.js
+++ script.js
@@ -4,61 +4,76 @@ var app = angular.module('myApp', []);
 
 // Mendefinisikan controller untuk navbar
 app.controller('myCtrl', function($scope) {
-
     var username = localStorage.getItem('username');
-
     $scope.username = username;
 });
+
+const swiperWrapper = document.querySelector(".swiper-wrapper"); // Menambahkan ini di sini untuk mengakses elemen swiper-wrapper
 
 document.addEventListener("DOMContentLoaded", function () {
     // Mendapatkan data dari file JSON
     fetch("recipes.json")
-        .then(response => response.json())
-        .then(data => {
-            const foodData = data;
-            const swiperWrapper = document.querySelector(".swiper-wrapper");
-            const links = document.querySelectorAll(".categories__item a");
+    .then(response => response.json())
+    .then(data => {
+        const foodData = data;
 
-// Menampilkan semua data pada awalnya
-displayFoodData(foodData);
+        // Memanggil displayFoodData dengan semua data makanan saat halaman dimuat pertama kali
+        displayFoodData(foodData);
 
-links.forEach((link) => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
-        const category = e.target.dataset.id;
-        let filteredFood;
-        if (category === "All") {
-            // Tampilkan semua data makanan jika kategori "All" dipilih
-            filteredFood = foodData;
-        } else {
-            // Filter data berdasarkan kategori yang dipilih
-            filteredFood = foodData.filter((item) => item.category === category);
-        }
-        displayFoodData(filteredFood);
-    });
-});
+        // Inisialisasi Swiper untuk konten default saat halaman dimuat
+        initializeSwiper();
 
-function displayFoodData(food) {
-    let displayData = food.map((cat_items) => {
-        return `<div class="swiper-slide">
-                <div class="card">
-                        <a href="${cat_items.url}" target="_blank">
-                            <img src="${cat_items.img}">
-                            <h5>${cat_items.title}</h5>
-                            <div class="time row">
-                                <div class="row">
-                                    <i class="fa fa-clock-o" style="font-size:15px"><p>${cat_items.time}</p></i>   <i class="fa fa-cutlery" style="font-size:15px"><p>${cat_items.category}</p></i>
-                                </div>
+        // Array untuk menyimpan instance Swiper untuk setiap kategori
+        const links = document.querySelectorAll(".categories__item a");
+
+        links.forEach((link) => {
+            link.addEventListener("click", (e) => {
+                e.preventDefault();
+                const category = e.target.dataset.id;
+                let filteredFood;
+
+                if (category === "All") {
+                    // Tampilkan semua data makanan jika kategori "All" dipilih
+                    displayFoodData(foodData); // Menampilkan semua data tanpa filter
+                } else {
+                    // Filter data berdasarkan kategori yang dipilih
+                    filteredFood = foodData.filter((item) => item.category === category);
+                    displayFoodData(filteredFood);
+                }
+            });
+        });
+
+        function displayFoodData(food) {
+            let displayData = food.map((cat_items) => {
+                return `<div class="swiper-slide">
+                            <div class="card">
+                                <a href="${cat_items.url}" target="_blank">
+                                    <img src="${cat_items.img}">
+                                    <h5>${cat_items.title}</h5>
+                                    <div class="time row">
+                                        <div class="row">
+                                            <i class="fa fa-clock-o" style="font-size:15px"><p>${cat_items.time}</p></i>   <i class="fa fa-cutlery" style="font-size:15px"><p>${cat_items.category}</p></i>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
-                        </a>
-                    </div>
-                </div>`; 
-    });
-    displayData = displayData.join("");
-    swiperWrapper.innerHTML = displayData;
-}
+                        </div>`; 
+            });
+            // Menampilkan data pada swiper
+            swiperWrapper.innerHTML = displayData.join("");
+        }
 
-
+        function initializeSwiper() {
+            var swiper = new Swiper(".mySwiper", {
+                slidesPerView: 4,
+                spaceBetween: 10,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            });
+        }
+        
             // Mendefinisikan fungsi separateByNationality
 function separateByNationality(food) {
     const separatedFoods = {
@@ -232,14 +247,14 @@ function displayFeaturedFoodData(food) {
 // Menampilkan makanan berdasarkan properti featured
 displayFeaturedFoodData(foodData);
 
-var swiperFeatured = new Swiper(".featured .swiper", {
-    slidesPerView: 4,
-    spaceBetween: 10,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
+    var swiperFeatured = new Swiper(".featured .swiper", {
+        slidesPerView: 4,
+        spaceBetween: 10,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
 
         })
         .catch(error => console.log("Error fetching data:", error));
