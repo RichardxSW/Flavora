35105efa54ce3d5ae7f16c261443df06635060b0
TanjayaJason <tanjaya.jason01@gmail.com>
2024-03-26 16:54:17 +0700

    bikin auth

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require("cors");
+const passportSetup = require("./passport");
+const passport = require("passport");
+const authRoute = require("./routes/auth")
 const expressLayouts = require("express-ejs-layouts");
+const cookieSession = require("cookie-session");
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(expressLayouts); 
+
+app.use(
+    cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
+  );
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/auth", authRoute);
+
+app.use(
+    cors({
+      origin: "http://localhost:5000",
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true,
+    })
+);
 
 app.get('/', (req, res) => {
     res.render('login.ejs', {title: 'Login', layout: "accountlayout"});
---
+++ passport.js
@@ -0,0 +1,22 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require('passport');
+const GOOGLE_CLIENT_ID = "103328795113-crtgk3olggmheoqr0fnklvgrv2ak898q.apps.googleusercontent.com"
+const GOOGLE_CLIENT_SECRET = "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"
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    done(null, profile)
+  }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
