465ace60a6457ce354ac5b6d1c3e59414f3a6f12
TanjayaJason <tanjaya.jason01@gmail.com>
2024-03-27 16:35:12 +0700

    bikin login facebook

--- passport.js
+++ passport.js
@@ -1,4 +1,5 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const passport = require('passport');
 const GOOGLE_CLIENT_ID = "103328795113-crtgk3olggmheoqr0fnklvgrv2ak898q.apps.googleusercontent.com"
 const GOOGLE_CLIENT_SECRET = "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"
@@ -12,6 +13,17 @@ passport.use(new GoogleStrategy({
   function(request, accessToken, refreshToken, profile, done) {
     done(null, profile)
   }
+));
+
+passport.use(new FacebookStrategy({
+  clientID: FACEBOOK_APP_ID,
+  clientSecret: FACEBOOK_APP_SECRET,
+  callbackURL: "/auth/facebook/callback",
+  passReqToCallback: true
+},
+function(request, accessToken, refreshToken, profile, done) {
+  done(null, profile)
+}
 ));
 
 passport.serializeUser((user, done) => {
--- routes/auth.js
+++ routes/auth.js
@@ -36,22 +36,12 @@ router.get(
   })
 );
 
-router.get("/github", passport.authenticate("github", { scope: ["profile"] }));
-
-router.get(
-  "/github/callback",
-  passport.authenticate("github", {
-    successRedirect: CLIENT_URL,
-    failureRedirect: "/login/failed",
-  })
-);
-
 router.get("/facebook", passport.authenticate("facebook", { scope: ["profile"] }));
 
 router.get(
   "/facebook/callback",
   passport.authenticate("facebook", {
-    successRedirect: "http://localhost:3000/home",
+    successRedirect: "/home",
     failureRedirect: "/login/failed",
   })
 );
