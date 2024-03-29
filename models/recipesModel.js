8ca7ff5847ec6745e349bae491fb6b0520d9314a
Richard Souwiko <richard.s050804@gmail.com>
2024-03-29 11:52:52 +0700

    create recipesModel for schema

---
+++ models/recipesModel.js
@@ -0,0 +1,87 @@
+const {Schema, model} = require('mongoose')
+
+const recipesSchema = new Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: [String],
+        required: true
+    },
+    nationality: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: String
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    serving: {
+        type: String,
+        required: true
+    },
+    length: {
+        type: String,
+        required: true
+    },
+    minutes: {
+        type: String,
+        required: true
+    },
+    calories: {
+        type: String,
+        required: true
+    },
+    bahan: {
+        type: [String],
+        required: true
+    },
+    cara: {
+        type: [String],
+        required: true
+    },
+    name: {
+        type: [String],
+        required: true
+    },
+    photo: {
+        type: [String],
+        required: true
+    },
+    rating: {
+        type: [String],
+        required: true
+    },
+    date: {
+        type: [String],
+        required: true
+    },
+    review: {
+        type: [String],
+        required: true
+    }
+})
+
+
+
+module.exports = model('Recipe', recipesSchema)
