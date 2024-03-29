6932c61f0888724a2f7b41771e90fed09981ed1a
Richard Souwiko <richard.s050804@gmail.com>
2024-03-29 11:52:15 +0700

    add new code for rest api mongodb using mongoose

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const cors = require("cors");
 const passportSetup = require("./passport");
 const passport = require("passport");
 const authRoute = require("./routes/auth")
 const expressLayouts = require("express-ejs-layouts");
 const cookieSession = require("cookie-session");
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(expressLayouts); 
+app.use(express.json());
+app.use(morgan('dev'));
+app.use('/api/recipes' ,require("./routes/api/recipesAPI"))
+
+mongoose.connect(MONGO_URL)
+    .then(() => console.log(`MongoDB connected at ${MONGO_URL}`))
+    .catch(err => console.log(err))
 
 app.use(
     cookieSession({ name: "session", keys: ["lama"], maxAge: 24 * 60 * 60 * 100 })
@@ -52,6 +65,6 @@ app.get('/pinned', (req, res) => {
     res.render('pinned', {title: 'Pinned', layout: "mainlayout", name: req.user.displayName, pic: req.user.photos[0].value});
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
