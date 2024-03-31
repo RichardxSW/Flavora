cb6a5a184134b9826d41214e559a763f12f76250
Richard Souwiko <richard.s050804@gmail.com>
2024-03-31 15:25:38 +0700

    deactive reviewModel

---
+++ models/reviewModel.js
@@ -0,0 +1,32 @@
+// const {Schema, model} = require('mongoose')
+
+// const reviewSchema = new Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     photo: {
+//         type: String,
+//         required: true
+//     },
+//     rating: {
+//         type: String,
+//         required: true
+//     },
+//     date: {
+//         type: String,
+//         required: true
+//     },
+//     review: {
+//         type: String,
+//         required: true
+//     },
+//     recipe_ID: {
+//         type: Schema.Types.ObjectId,
+//         ref: 'Recipe'
+//     }
+// })
+
+
+
+// module.exports = model('Review', reviewSchema)
