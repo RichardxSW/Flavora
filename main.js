2d24c290d212b896c3710f2c18053df18f2b9f23
Richard Souwiko <richard.s050804@gmail.com>
2024-04-13 11:12:32 +0700

    add admin auth for account , render dashboard and addRecipe

--- main.js
+++ main.js
@@ -170,6 +170,7 @@ app.get('/home', isAuthenticated, async (req, res) => {
                 recipes: recipes, 
                 name: name, // Nama pengguna
                 pic: pic, // URL gambar profil
+                isAdmin: req.user.isAdmin,
                 title: 'Home', 
                 layout: "mainlayout"
             });
@@ -224,6 +225,7 @@ app.get('/search', async (req, res) => {
                 filteredRecipes: filteredRecipes,
                 name: name,
                 pic: pic,
+                isAdmin: req.user.isAdmin,
                 title: 'Search',
                 layout: "mainlayout",
             });
@@ -259,6 +261,7 @@ app.get('/detail/:recipeID', async (req, res) => {
                 relatedRecipes: relatedRecipes, 
                 name: name, 
                 pic: pic, 
+                isAdmin: req.user.isAdmin,
                 title: 'Detail', 
                 layout: "mainlayout"})
         } else {
@@ -333,7 +336,8 @@ app.get('/recent' ,async (req, res) => {
             recipes: recipes, 
             title: 'Recent',  
             name: name, 
-            pic: pic, 
+            pic: pic,
+            isAdmin: req.user.isAdmin, 
             layout: "mainlayout"});
         } else {
             res.status(404).send("Recipe not found")
@@ -364,6 +368,7 @@ app.get('/pinned', async(req, res) => {
             layout: "mainlayout", 
             name: name, 
             pic: pic,
+            isAdmin: req.user.isAdmin,
 });
         } else {
             res.status(404).send("Recipe not found")
@@ -372,6 +377,122 @@ app.get('/pinned', async(req, res) => {
             res.status(500).send("Internal Server Error")
         }
     });
+
+app.get('/dashboard', async(req, res) => {
+    try {
+        const recipes = await Recipes.find();
+        if (recipes) {
+            let name = '';
+            let pic = '';
+            if (req.user) { 
+                if (req.user.username) { 
+                    name = req.user.username || ''; 
+                    pic = '/img/profilepic.jpg'; 
+                } else {
+                    name = req.user.displayName || '';
+                    pic = req.user.profilePicture || '';
+                }
+            }
+            res.render('dashboard', {
+            recipes: recipes, 
+            title: 'Dashboard', 
+            layout: "mainlayout", 
+            name: name, 
+            pic: pic,
+            isAdmin: req.user.isAdmin,
+});
+        } else {
+            res.status(404).send("Recipe not found")
+        }
+        } catch (error) { 
+            res.status(500).send("Internal Server Error")
+        }
+    });
+
+app.get('/addRecipe', async(req, res) => {
+    try {
+        const recipes = await Recipes.find();
+        if (recipes) {
+            let name = '';
+            let pic = '';
+            if (req.user) { 
+                if (req.user.username) { 
+                    name = req.user.username || ''; 
+                    pic = '/img/profilepic.jpg'; 
+                } else {
+                    name = req.user.displayName || '';
+                    pic = req.user.profilePicture || '';
+                }
+            }
+            res.render('addRecipe', {
+            recipes: recipes, 
+            title: 'Add new Recipe', 
+            layout: "mainlayout", 
+            name: name, 
+            pic: pic,
+            isAdmin: req.user.isAdmin,
+});
+        } else {
+            res.status(404).send("Recipe not found")
+        }
+        } catch (error) { 
+            res.status(500).send("Internal Server Error")
+        }
+    });
+
+    app.post('/addRecipe', async (req, res) => {
+        try {
+            const lastRecipe = await Recipes.findOne().sort({ recipeID: -1 });
+            const recipes = await Recipes.find();
+        let recipeID;
+
+        if (lastRecipe) {
+            // Jika ada recipe terakhir, tambahkan 1 ke recipeID terakhir
+            recipeID = lastRecipe.recipeID + 1;
+        } else {
+            // Jika tidak ada recipe, set recipeID menjadi 1
+            recipeID = 1;
+        }
+            const {
+                title,
+                category,
+                nationality,
+                featured,
+                // img,
+                desc,
+                serving,
+                length,
+                minutes,
+                calories,
+                bahan,
+                cara 
+            } = req.body;
+
+            const time = minutes + ' Minutes';
+
+            const newRecipe = new Recipes({
+                recipeID,
+                title,
+                category,
+                nationality,
+                featured,
+                // img,
+                desc,
+                serving,
+                length,
+                time,
+                minutes,
+                calories,
+                bahan,
+                cara
+            });
+            await newRecipe.save();
+            res.status(201).send("Recipe added successfully");
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal Server Error");
+        }
+    })
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
