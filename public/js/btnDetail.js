cf85320a757c1906ab8232717acdbc2e9b0e8e40
Richard Souwiko <richard.s050804@gmail.com>
2024-04-13 14:12:42 +0700

    disable btnDetail

--- public/js/btnDetail.js
+++ public/js/btnDetail.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const btnSave = document.querySelector(".unsaved");
-    const btnUnsave = document.querySelector(".saved");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const btnSave = document.querySelector(".unsaved");
+//     const btnUnsave = document.querySelector(".saved");
 
-    btnSave.addEventListener("click", function () {
-        btnSave.style.display = "none";
-        btnUnsave.style.display = "inline-block"
-    })
+//     btnSave.addEventListener("click", function () {
+//         btnSave.style.display = "none";
+//         btnUnsave.style.display = "inline-block"
+//     })
 
-    btnUnsave.addEventListener("click", function () {
-        btnUnsave.style.display = "none";
-        btnSave.style.display = "inline-block";
-    })
-});
+//     btnUnsave.addEventListener("click", function () {
+//         btnUnsave.style.display = "none";
+//         btnSave.style.display = "inline-block";
+//     })
+// });
 
-var app = angular.module('myApp', []);
+// var app = angular.module('myApp', []);
 
-app.controller('myCtrl', function($scope) {
-    var username = localStorage.getItem('username');
-    $scope.username = username;
-});
+// app.controller('myCtrl', function($scope) {
+//     var username = localStorage.getItem('username');
+//     $scope.username = username;
+// });
