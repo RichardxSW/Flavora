e9f7a450c68db3fffc820347e25e8075fc424e6b
Richard Souwiko <richard.s050804@gmail.com>
2024-05-04 15:24:31 +0700

    add routing review

--- main.js
+++ main.js
@@ -18,7 +18,8 @@ const LocalUser = require("./models/localuserModel");
 const bcrypt = require("bcrypt");
 const multer = require('multer');
 const path = require('path');
-const adminRouter = require('./routes/api/admin'); // Ganti dengan path yang sesuai ke file homepage.js Anda
+const adminRouter = require('./routes/api/admin'); 
+const reviewRouter = require('./routes/api/review'); 
 dotenv.config();
 
 // Import middleware from routes/api
@@ -64,6 +65,7 @@ app.use(redirectTrailingSlash);
 app.use(checkUser);
 app.use('/', require('./routes/api/account'));
 app.use('/admin', adminRouter);
+app.use('/review', reviewRouter);
 
 // MongoDB Connection
 mongoose.connect(MONGO_URL)
@@ -418,96 +420,6 @@ app.get('/detail/:recipeID', isAuthenticated, async (req, res) => {
         res.status(500).send("Internal Server Error")
     }
 })
-
-app.post('/postReview/:recipeID', async (req, res) => {
-    try {
-        const { recipeID } = req.params;
-        const { rating, review, date, name , photo } = req.body;
-
-        // Lakukan sesuatu dengan data yang diterima, misalnya menyimpan ke database menggunakan Mongoose
-        // Contoh:
-        const recipe = await Recipes.findOne({ recipeID });
-        if (!recipe) {
-            return res.status(404).send("Recipe not found");
-        }
-        // Menambahkan review ke resep
-        recipe.reviews.push({
-            rating,
-            review,
-            date,
-            name,
-            photo
-        });
-
-        // Hitung totalRating, totalReviews, dan averageRating yang baru
-        const totalReviews = recipe.reviews.length;
-        let totalRating = 0;
-        let averageRating = 0;
-        if (totalReviews > 0) {
-            for (let i = 0; i < totalReviews; i++) {
-                totalRating += parseInt(recipe.reviews[i].rating);
-            }
-            averageRating = totalRating / totalReviews;
-            averageRating = averageRating.toFixed(1);
-        }
-
-        // Simpan totalReviews dan averageRating ke dalam dokumen resep
-        recipe.totalReviews = totalReviews;
-        recipe.averageRating = averageRating;
-
-        await recipe.save();
-
-        res.status(201).send("Review added successfully");
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-
-// Endpoint untuk menghapus komentar dari resep
-app.delete('/deleteComment/:recipeID/:commentID', async (req, res) => {
-    try {
-        // Ambil ID resep dan ID komentar dari parameter route
-        const recipeID = req.params.recipeID;
-        const commentID = req.params.commentID;
-
-        // Lakukan operasi penghapusan komentar dari resep di database
-        // Misalnya, menggunakan model Recipes
-        await Recipes.updateOne({ recipeID: recipeID }, { $pull: { reviews: { _id: commentID } } });
-
-        // Kirim respon yang berhasil
-        res.status(200).end();
-    } catch (error) {
-        // Tangani kesalahan jika terjadi
-        console.error('Error deleting comment:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-app.put('/editComment/:commentID', async (req, res) => {
-    try {
-        const commentID = req.params.commentID;
-        const { newComment } = req.body;
-
-        // Lakukan operasi pengeditan komentar di database (contoh: menggunakan model Recipes)
-        const updatedReview = await Recipes.findOneAndUpdate(
-            { 'reviews._id': commentID },
-            { $set: { 'reviews.$.review': newComment } },
-            { new: true }
-        );
-
-        if (!updatedReview) {
-            return res.status(404).json({ error: 'Review not found' });
-        }
-
-        // Kirim respon yang berhasil
-        res.status(200).json({ message: 'Comment updated successfully', updatedReview });
-    } catch (error) {
-        // Tangani kesalahan jika terjadi
-        console.error('Error editing comment:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
 
 app.post('/pinrecipe', async (req, res) => {
     try {
