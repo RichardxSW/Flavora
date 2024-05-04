d1996ffa7daecd8532e8d48db8786bb5b59280b8
TanjayaJason <tanjaya.jason01@gmail.com>
2024-05-04 15:28:01 +0700

    clean code main js (profile and edit)

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ dotenv.config();
 
 // Import middleware from routes/api
 const { createAdmin, checkUser, redirectTrailingSlash, isAuthenticated, isLoggedIn } = require('./routes/api/middleware');
+const profileRouter = require('./routes/api/profile');
 
 // Port dan URL MongoDB
 const PORT = process.env.PORT || 3000;
@@ -64,6 +65,7 @@ app.use(redirectTrailingSlash);
 app.use(checkUser);
 app.use('/', require('./routes/api/account'));
 app.use('/admin', adminRouter);
+app.use('/', profileRouter);
 
 // MongoDB Connection
 mongoose.connect(MONGO_URL)
@@ -93,144 +95,6 @@ mongoose.connect(MONGO_URL)
         }
     })
     .catch(err => console.log(err))
-
-app.get('/profile', (req, res) => {
-    let name = '';
-    let email = '';
-    let password = '';
-    let userData = req.session.freshUserData || {}; // Inisialisasi objek userData
-    let errorMsg = req.flash('error');
-    let successMsg = req.flash('success');
-
-    if (!req.user) {
-        // Jika pengguna belum login, hapus session.freshUserData jika ada
-        if (req.session.freshUserData) {
-            delete req.session.freshUserData;
-        }
-    } else {
-        // Jika pengguna sudah login
-        if (!userData || Object.keys(userData).length === 0) {
-            // Jika userData kosong, isi dengan data pengguna dari req.user
-            if (req.user.username) { 
-                userData = {
-                    name: req.user.username || '', 
-                    profilePicture: req.user.profilePicture,
-                    email: req.user.email || '',
-                    password: maskPassword(req.user.password || ''),
-                    _id: req.user._id
-                };
-            } else {
-                userData = {
-                    name: req.user.displayName || '',
-                    profilePicture: req.user.profilePicture || '',
-                    email: req.user.email || '',
-                    password: maskPassword(req.user.password || ''),
-                    _id: req.user._id
-                };
-            }
-        } else {
-            // Jika userData sudah terisi, ubah nama-nama properti sesuai keinginan
-            userData.name = userData.username;
-            userData.password = maskPassword(userData.password || ''); // Ubah password menjadi masked version
-        }
-    }
-
-    res.render('profile', {
-        user: userData,
-        errorMsg: errorMsg,
-        successMsg: successMsg,
-        title: 'Profile', 
-        layout: 'accountLayout'
-    });
-});
-
-function maskPassword(password) {
-    return '*'.repeat(password.length);
-}
-
-app.get('/edit', isAuthenticated, async(req, res) => {
-    try {
-        const id = req.query.id;
-        const userData = await LocalUser.findById(id);
-
-        if(userData){
-            res.render('edit', {
-                user: userData,
-                title: 'Edit', 
-                layout: "accountLayout"});
-        }
-        else{
-            req.flash('errorMsg', 'Account can not be modified.');
-            res.redirect('/profile');
-        }
-    } catch (error) {
-        console.log(error.message);
-    }
-});
-
-// Konfigurasi penyimpanan file dengan multer
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '/public/userImages')) // Menentukan direktori penyimpanan file
-    },
-    filename: function (req, file, cb) {
-        const name = Date.now() + '-' + file.originalname;
-        cb(null, name) // Menentukan nama file yang diunggah
-    }
-});
-
-// Inisialisasi multer dengan konfigurasi penyimpanan
-const upload = multer({ storage: storage });
-
-app.post('/edit', upload.single('image'), async(req, res) => {
-    try {
-        let userData = {};
-        const id = req.body.user_id;
-
-        if (req.file) {
-            // Jika ada file yang diunggah, simpan informasi file ke dalam userData
-            userData = {
-                username: req.body.name,
-                email: req.body.email,
-                password: req.body.password,
-                profilePicture: req.file.filename // Gunakan req.file.filename untuk mendapatkan nama file yang disimpan oleh multer
-            };
-
-            // Menghapus foto lama jika ada dan bukan 'profilepic.jpg'
-            const oldUserData = await LocalUser.findById(id);
-            if (oldUserData.profilePicture && oldUserData.profilePicture !== 'profilepic.jpg') {
-                const oldImagePath = path.join(__dirname, '/public/userImages', oldUserData.profilePicture);
-                if (fs.existsSync(oldImagePath)) {
-                    fs.unlinkSync(oldImagePath);
-                    console.log(`File ${oldUserData.profilePicture} telah dihapus.`);
-                }
-            }
-        } else {
-            // Jika tidak ada file yang diunggah, hanya simpan informasi pengguna
-            userData = {
-                username: req.body.name,
-                email: req.body.email,
-                password: req.body.password
-            };
-        }
-
-        // Update data pengguna di MongoDB
-        const updatedUserData = await LocalUser.findByIdAndUpdate(id, { $set: userData }, { new: true });
-
-        // Ambil data terbaru dari MongoDB
-        const freshUserData = await LocalUser.findById(id);
-
-        // Simpan freshUserData di dalam sesi atau cookie
-        req.session.freshUserData = freshUserData;
-
-        // Redirect ke halaman profil setelah berhasil update
-        req.flash('successMsg', 'Account updated successfully.');
-        res.redirect('/profile');
-
-    } catch (error) { 
-        console.log(error.message);
-    }
-});
 
 app.get('/home', checkUser, isAuthenticated, async (req, res) => {
     try {
---
+++ routes/api/profile.js
@@ -0,0 +1,139 @@
+const {Router} = require('express');
+const multer = require('multer');
+const LocalUser = require("../../models/localuserModel");
+const profileRouter = Router();
+
+profileRouter.get('/profile', (req, res) => {
+    let userData = req.session.freshUserData || {}; // Inisialisasi objek userData
+    let errorMsg = req.flash('error');
+    let successMsg = req.flash('success');
+
+    if (!req.user) {
+        // Jika pengguna belum login, hapus session.freshUserData jika ada
+        if (req.session.freshUserData) {
+            delete req.session.freshUserData;
+        }
+    } else {
+        // Jika pengguna sudah login
+        if (!userData || Object.keys(userData).length === 0) {
+            // Jika userData kosong, isi dengan data pengguna dari req.user
+            if (req.user.username) { 
+                userData = {
+                    name: req.user.username || '', 
+                    profilePicture: req.user.profilePicture,
+                    email: req.user.email || '',
+                    password: maskPassword(req.user.password || ''),
+                    _id: req.user._id
+                };
+            } else {
+                userData = {
+                    name: req.user.displayName || '',
+                    profilePicture: req.user.profilePicture || '',
+                    email: req.user.email || '',
+                    password: maskPassword(req.user.password || ''),
+                    _id: req.user._id
+                };
+            }
+        } else {
+            // Jika userData sudah terisi, ubah nama-nama properti sesuai keinginan
+            userData.name = userData.username;
+            userData.password = maskPassword(userData.password || ''); // Ubah password menjadi masked version
+        }
+    }
+
+    res.render('profile', {
+        user: userData,
+        errorMsg: errorMsg,
+        successMsg: successMsg,
+        title: 'Profile', 
+        layout: 'accountLayout'
+    });
+});
+
+function maskPassword(password) {
+    return '*'.repeat(password.length);
+}
+
+profileRouter.get('/edit', async(req, res) => {
+    try {
+        const id = req.query.id;
+        const userData = await LocalUser.findById(id);
+
+        if(userData){
+            res.render('edit', {
+                user: userData,
+                title: 'Edit', 
+                layout: "accountLayout"});
+        }
+        else{
+            req.flash('errorMsg', 'Account can not be modified.');
+            res.redirect('/profile');
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+// Konfigurasi penyimpanan file dengan multer
+const storage = multer.diskStorage({
+    destination:  './public/userImages', // Menentukan direktori penyimpanan file
+    filename: function (req, file, cb) {
+        const name = Date.now() + '-' + file.originalname;
+        cb(null, name) // Menentukan nama file yang diunggah
+    }
+});
+
+// Inisialisasi multer dengan konfigurasi penyimpanan
+const upload = multer({ storage: storage });
+
+profileRouter.post('/edit', upload.single('image'), async(req, res) => {
+    try {
+        let userData = {};
+        const id = req.body.user_id;
+
+        if (req.file) {
+            // Jika ada file yang diunggah, simpan informasi file ke dalam userData
+            userData = {
+                username: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                profilePicture: req.file.filename // Gunakan req.file.filename untuk mendapatkan nama file yang disimpan oleh multer
+            };
+
+            // Menghapus foto lama jika ada dan bukan 'profilepic.jpg'
+            const oldUserData = await LocalUser.findById(id);
+            if (oldUserData.profilePicture && oldUserData.profilePicture !== 'profilepic.jpg') {
+                const oldImagePath = path.join(__dirname, '/public/userImages', oldUserData.profilePicture);
+                if (fs.existsSync(oldImagePath)) {
+                    fs.unlinkSync(oldImagePath);
+                    console.log(`File ${oldUserData.profilePicture} telah dihapus.`);
+                }
+            }
+        } else {
+            // Jika tidak ada file yang diunggah, hanya simpan informasi pengguna
+            userData = {
+                username: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            };
+        }
+
+        // Update data pengguna di MongoDB
+        const updatedUserData = await LocalUser.findByIdAndUpdate(id, { $set: userData }, { new: true });
+
+        // Ambil data terbaru dari MongoDB
+        const freshUserData = await LocalUser.findById(id);
+
+        // Simpan freshUserData di dalam sesi atau cookie
+        req.session.freshUserData = freshUserData;
+
+        // Redirect ke halaman profil setelah berhasil update
+        req.flash('successMsg', 'Account updated successfully.');
+        res.redirect('/profile');
+
+    } catch (error) { 
+        console.log(error.message);
+    }
+});
+
+module.exports = profileRouter;
